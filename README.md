8b791c69270e442dd04ff152943ec5a38b1caaa7
Javier Donoso <javier.donoso@nuilea.com>
2023-06-15 12:53:03 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+
+# Gelato Web3 functions <<-->> RedStone PoC
+
+## Summary
+
+RedStone Oracle that using Pyth Network and Gelato Web3 functions to:
+- Push on-chain price if the price difference between stored and live price is gerater than 20%
+
+## Demo
+- Mumbai:
+  - Smart Contract: [https://mumbai.polygonscan.com/address/0xb26a01df1913a9f1e9cdbaed240e8a38f724a673#readContract](https://mumbai.polygonscan.com/address/0xb26a01df1913a9f1e9cdbaed240e8a38f724a673#readContract)
+  - Web3 Function: [https://beta.app.gelato.network/task/0xe5647565edb71d22583c74085b4dccd7e0c202569faa542caf99fd3d0d1d533d?chainId=80001](https://beta.app.gelato.network/task/0xe5647565edb71d22583c74085b4dccd7e0c202569faa542caf99fd3d0d1d533d?chainId=80001)
+
+## Deploy your smart contract and web3 function
+```
+yarn run deploy 
+```
+
+## How to run
+
+1. Install project dependencies:
+```
+yarn install
+```
+
+2. Create a `.env` file with your private config:
+```
+cp .env.example .env
+```
+You will need to input your `PROVIDER_URL`, your RPC.
+
+
+3. Test the  web3 function
+```
+npx w3f test src/web3-functions/redstone/index.ts --logs --chain-id=80001
+```
+
+4. Deploy the web3 function on IPFS
+```
+npx w3f deploy src/web3-functions/redstone/index.ts
+```
+
+ ✓ Web3Function deployed to ipfs.
+ ✓ CID: QmWz7Y4fpoTxrXsHe1MK9qnk9bNUZrapMGGYiiEcm995JE
+
+5. Create the task following the link provided when deploying the web3 to IPFS in our case:
+```
+https://beta.app.gelato.network/new-task?cid=QmWz7Y4fpoTxrXsHe1MK9qnk9bNUZrapMGGYiiEcm995JE
+```
---
+++ test/RedstoneOracle.test.ts
@@ -0,0 +1,61 @@
+import hre,{ ethers, deployments, w3f } from "hardhat";
+import { expect } from "chai";
+import { RedstoneOracle } from "../typechain";
+import { before } from "mocha";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { time } from "@nomicfoundation/hardhat-network-helpers";
+import {
+  Web3FunctionResultV2,
+  Web3FunctionUserArgs,
+} from "@gelatonetwork/web3-functions-sdk";
+import { Web3FunctionHardhat } from "@gelatonetwork/web3-functions-sdk/hardhat-plugin";
+
+
+describe("RedstoneOracle Tests", function () {
+  this.timeout(0);
+
+  let owner: SignerWithAddress;
+
+  let oracle: RedstoneOracle;
+  let oracleAddress: string;
+  let oracleW3f: Web3FunctionHardhat;
+  let userArgs: Web3FunctionUserArgs;
+
+  before(async function () {
+    await deployments.fixture();
+    await time.increaseTo(Math.floor(Date.now() / 1000));
+
+    [owner] = await hre.ethers.getSigners();
+    oracleAddress = (await deployments.get("RedstoneOracle")).address;
+    oracle = await ethers.getContractAt("RedstoneOracle",oracleAddress) as RedstoneOracle;
+    oracleW3f = w3f.get("redstone");
+
+    userArgs = {
+      currency: "ETH",
+      oracleAddress: oracleAddress,
+    };
+  });
+
+  it("Update oracle price on first execution", async () => {
+    let { result } = await oracleW3f.run({ userArgs });
+    result = result as Web3FunctionResultV2;
+
+    expect(result.canExec).to.equal(true);
+    if (!result.canExec) throw new Error("!result.canExec");
+
+    const calldata = result.callData[0];
+    await owner.sendTransaction({ to: oracleAddress, data: calldata.data });
+
+    const storedPrice = await oracle.getStoredPrice();
+    expect(storedPrice.gt(0)).to.be.true;
+  });
+
+  it("Don't update when deviation is too small", async () => {
+    const { result } = await oracleW3f.run({ userArgs });
+    expect(result.canExec).to.equal(false);
+    if (result.canExec) throw new Error("result.canExec");
+
+    const message = result.message;
+    expect(message).to.equal("No update: price deviation too small");
+  });
+});
